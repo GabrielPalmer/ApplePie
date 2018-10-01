5ab9b8eb1f17c9b6b917c4b9ceb2f8492047fcc4
Gabriel Palmer <gabepalmer@gabriels-macbook-air.local>
2018-10-01 07:33:32 -0600

    round log complete

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var logTableView: UITableView!
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,7 +24,10 @@ class ViewController: UIViewController {
     var totalWins = 0
     var totalLosses = 0
     
-    var listOfWords: [String] = ["piano", "box", "fish", "lightning", "table", "fire", "street", "cold", "book", "knife", "candy", "game", "clock", "music", "food", "sock", "sun", "picture", "chair", "wind", "computer", "plant", "dirt", "glass", "door", "mountain", "flower", "jazz", "power", "water", "hand", "hair", "potato", "cool", "color", "camping", "television", "icosahedron", "truck", "road", "plane", "wheel", "letter", "coordinate", "bullet", "jump", "battery", "shell", "fart", "beard", "machine", "stomach", "backpack", "fan", "board", "laser", "pie", "fruit", "stop", "grass", "keychain", "pen", "question", "bone", "lazy", "desk", "call", "error", "knot", "rip", "ocean", "crash", "guacamole", "beam", "season", "exit", "shelf", "space", "bag", "tooth", "strength", "bottle", "plumber", "wire", "ground", "person", "flip", "tag", "message", "glue", "broom", "bucket", "sink", "cut", "burn", "bush", "dumbledore", "alabama", "mississippi"]
+    var logEntries: [String] = []
+    var roundsWon: [Bool] = []
+    
+    var listOfWords: [String] = ["piano", "box", "fish", "lightning", "table", "fire", "street", "cold", "book", "knife", "candy", "game", "clock", "music", "food", "sock", "sun", "picture", "chair", "wind", "computer", "plant", "dirt", "glass", "door", "mountain", "flower", "jazz", "power", "water", "hand", "hair", "potato", "cool", "color", "camping", "television", "icosahedron", "truck", "road", "plane", "wheel", "letter", "coordinate", "bullet", "jump", "battery", "shell", "fart", "beard", "machine", "stomach", "backpack", "fan", "board", "laser", "pie", "fruit", "stop", "grass", "keychain", "pen", "question", "bone", "lazy", "desk", "call", "error", "knot", "rip", "ocean", "crash", "guacamole", "beam", "season", "exit", "shelf", "space", "bag", "tooth", "strength", "bottle", "plumber", "wire", "ground", "person", "flip", "tag", "message", "glue", "broom", "bucket", "sink", "cut", "burn", "bush", "gas", "alabama", "mississippi"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +36,8 @@ class ViewController: UIViewController {
         newRound()
     }
 
+    //MARK: Actions
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
@@ -37,6 +45,29 @@ class ViewController: UIViewController {
         currentGame.playerGuessed(letter: letter)
         updateGameState()
     }
+    
+    //MARK: TableView Functions
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if logEntries.count > 12 {
+            logEntries.removeFirst()
+            roundsWon.removeFirst()
+        }
+        return logEntries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        
+        let word = logEntries[indexPath.row]
+        cell.textLabel?.text = word
+        cell.textLabel?.textColor = roundsWon[indexPath.row] ? UIColor.green : UIColor.red
+        
+        return cell
+    }
+    
+    //MARK: Custom Functions
     
     func updateUI() {
         
@@ -49,12 +80,19 @@ class ViewController: UIViewController {
     func updateGameState() {
         
         if currentGame.incorrectMovesRemaining == 0 {
+            
             totalLosses += 1
- 
+            logEntries.append(currentGame.word.capitalized)
+            roundsWon.append(false)
             newRound()
+            
         } else if currentGame.word == currentGame.formattedWord.replacingOccurrences(of: " ", with: ""){
+            
             totalWins += 1
+            logEntries.append(currentGame.word.capitalized)
+            roundsWon.append(true)
             newRound()
+            
         } else {
             updateUI()
         }
@@ -62,12 +100,17 @@ class ViewController: UIViewController {
     
     func newRound() {
         
+        logTableView.reloadData()
+        
         if !listOfWords.isEmpty {
+            
             let newWord = listOfWords.remove(at: Int(arc4random_uniform(UInt32(listOfWords.count))))
             currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
             enableLetterButtons(true)
             updateUI()
+            
         } else {
+            
             enableLetterButtons(false)
             correctWordLabel.text = "<out of words>"
             treeImageView.image = UIImage(named: "apples0")
